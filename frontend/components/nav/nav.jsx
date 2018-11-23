ee2d307285f64e1a30ed37e512d3ff877446ae72
Soon-Mi <ssoonmi@gmail.com>
2018-11-23 13:30:35 -0500

    Remove bars icon on the top left nav

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -3,10 +3,10 @@ import AuthSection from './auth_section';
 import NavSearchContainer from './nav_search_container';
 
 const Nav = (props) => {
+  // <div><i className="fas fa-bars fa-2x"></i></div>
   return (
     <nav>
       <div className="nav-left-section">
-        <div><i className="fas fa-bars fa-2x"></i></div>
         <i className="fab fa-youtube"></i>
         <a href='#/'><img src={window.logo}/></a>
       </div>
